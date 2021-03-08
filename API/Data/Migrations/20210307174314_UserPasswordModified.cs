89d6b4197cc17004aa12e907da7266171a0a5ca4
Douglas García <douglas.garcia.shaevka@gmail.com>
2021-03-07 20:03:45 -0500

    finished stage 3

---
+++ API/Data/Migrations/20210307174314_UserPasswordModified.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordModified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaswordHash",
+                table: "Users",
+                newName: "PasswordHash");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "PaswordHash");
+        }
+    }
+}
